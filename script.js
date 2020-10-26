c0953a6a237e34bbd0c4ad946ce8493ac72451b2
jswelsh <james.sinclair.welsh@gmail.com>
2020-10-25 19:19:06 -0700

    set axis and title for graph

--- script.js
+++ script.js
@@ -4,4 +4,23 @@ am4core.ready(function() {
   let chart = am4core.create("chartdiv", am4charts.XYChart);
   chart.padding(0, 0, 0, 0);
 
+  let title = chart.tooltipContainer.createChild(am4core.Label);
+  /* title.text = "TransLink"; */
+  title.fill = am4core.color("#00254b");
+  title.fontSize = 25;
+  title.width = am4core.percent(100);
+  title.textAlign = "middle";
+  title.x = 10;
+  title.y = 10;
+
+  function createAxis(list) {
+  let axis = list.push(new am4charts.ValueAxis());
+  axis.min = 0;
+  axis.max = 100;
+  axis.strictMinMax = true;
+  axis.renderer.grid.template.disabled = true;
+  axis.renderer.labels.template.disabled = true;
+  axis.renderer.baseGrid.disabled = true;
+}
+
 });
