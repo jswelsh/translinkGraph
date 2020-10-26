be8163221e79f7b42eba084df1f58e67bdb3a4ec
jswelsh <james.sinclair.welsh@gmail.com>
2020-10-25 19:15:58 -0700

    setting up the basic graph, using a translink pdf, as a wirefram for the graph pixels

---
+++ data.js
@@ -0,0 +1,31 @@
+/* 
+
+Waterfront
+Vancouver City Centre
+Yaletown–Roundhouse
+Olympic Village
+Broadway–City Hall
+King Edward
+Oakridge–41st Avenue
+Langara–49th Avenue
+Marine Drive
+Bridgeport
+Aberdeen
+Lansdowne
+Richmond–Brighouse
+
+Bridgeport
+Templeton
+Sea Island Centre
+YVR–Airport
+ */
+const translink =[
+  {
+    id:1,
+    name:'foo',
+    x:2,
+    y:1,
+    isWheelChairAccesible:true,
+    numberOfCompassTerminals:5
+  }
+]
